d763d922417c0b75f5ce5ba4225c5893be5d3eb6
Steven Bell <dev.thebells1111@gmail.com>
2023-01-19 15:48:16 -0600

    added navbars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,6 @@
 <script>
-	import { indexSearchResults } from '$/stores';
-	let searchQuery = 'able';
-	let searchInput;
-
-	async function searchIndex() {
-		console.log(searchQuery);
-
-		let url = `api/queryindex?q=${encodeURIComponent(
-			`/search/music/byterm?q=${searchQuery}&val=lightning`
-		)}`;
-
-		console.log(url);
-
-		const res = await fetch(url);
-		let data = await res.json();
-
-		try {
-			data = JSON.parse(data);
-
-			if (data.status) {
-				$indexSearchResults = data.feeds;
-			}
-		} catch (error) {}
-
-		console.log($indexSearchResults);
-	}
-
-	function checkEnter(e) {
-		if (e.key === 'Enter') {
-			searchIndex();
-		}
-	}
+	import { indexSearchResults, selectedAlbum } from '$/stores';
 </script>
-
-<input
-	bind:this={searchInput}
-	bind:value={searchQuery}
-	on:submit={searchIndex}
-	on:keypress={checkEnter}
-	on:focus={() => {
-		searchInput.select();
-	}}
-/>
-<button on:click={searchIndex}>Search</button>
 
 <ul>
 	{#each $indexSearchResults as result}
