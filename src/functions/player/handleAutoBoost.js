8073ec88622ff6b66b76346a613bb7da346e9b3b
Eric P <ericpp@gmail.com>
2025-02-22 11:13:07 -0600

    Add missing `webln` store to handleAutoBoost

--- src/functions/player/handleAutoBoost.js
+++ src/functions/player/handleAutoBoost.js
@@ -1,11 +1,13 @@
 import { get } from 'svelte/store';
 import sendBoost from '$functions/sendBoost';
 
-import { satsPerSong, user } from '$/stores';
+import { satsPerSong, user, webln } from '$/stores';
 
 async function handleAutoBoost(destinations) {
 	let $satsPerSong = get(satsPerSong);
 	let $user = get(user);
+	let $webln = get(webln);
+
 	if ($satsPerSong > 0 && destinations) {
 		try {
 			sendBoost({
